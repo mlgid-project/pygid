257b268649c1c617b34ddea84cfa7cbb74a5ad40
ainurabukaev99 <ainurabukaev@gmail.com>
2026-03-25 14:59:11 +0100

    converted_frame_num was fixed for loading from file

--- build/lib/pygid/__init__.py
+++
@@ -1,6 +0,0 @@
-from .expparams import ExpParams
-from .dataloader import DataLoader
-from .datasaver import DataSaver, ExpMetadata, SampleMetadata
-from .coordmaps import CoordMaps
-from .conversion import Conversion
-__version__ = "0.2.0"
