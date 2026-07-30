d5050805ce7d56930519f8b7fb804ecc008ce3a8
ainurabukaev99 <ainurabukaev@gmail.com>
2026-07-30 18:45:45 +0200

    tutorials update

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = ['_build', '**.ipynb_checkpoints']
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'pydata_sphinx_theme'
 html_static_path = ['_static']
 
 tutorial_dir = os.path.join(os.path.dirname(__file__), 'tutorials')
